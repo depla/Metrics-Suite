d9007cedb1520e15269c1887f3a4863431bc9207
sophannaek <sophanna.ek@student.csulb.edu>
2020-02-12 19:39:27 -0800

    added UI for startScreen and language selection

---
+++ metricsSuite/src/com/metricssuite/main.java
@@ -0,0 +1,18 @@
+/*
+    The application driver class
+ */
+
+package com.metricssuite;
+
+public class main {
+
+    public static void main(String[] args){
+        //start the application
+        init();
+    }
+
+    static void init(){
+        //initialize the startScreen
+        startScreen startscreen = new startScreen();
+    }
+}
