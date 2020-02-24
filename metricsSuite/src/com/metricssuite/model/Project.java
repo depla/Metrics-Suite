dfd2b6d5978c0a454f97977c188ea2a9482cdd3f
Dennis La <dennisla95@yahoo.com>
2020-02-24 09:24:31 -0800

    Update Project.java
    make project serializable and make methods to read/write, from/to file

--- metricsSuite/src/com/metricssuite/model/Project.java
+++ metricsSuite/src/com/metricssuite/model/Project.java
@@ -1,6 +1,8 @@
 package com.metricssuite.model;
 
-public class Project
+import java.io.*;
+
+public class Project implements Serializable
 {
     private String projectName;
     private String productName;
@@ -8,7 +10,7 @@ public class Project
     private String comments;
     private String language;
 
-    //private FunctionPoint functionPoint;
+    private FunctionPoint functionPoint;
 
 
     public Project(String projectName, String productName, String creatorName, String comments, String language)
@@ -18,6 +20,44 @@ public class Project
         this.creatorName = creatorName;
         this.comments = comments;
         this.language = language;
+        this.functionPoint = null;
+    }
+
+    /**
+     * Constructor to read in a project
+     * @param fileName the file name that contains the project
+     */
+    public Project(String fileName)
+    {
+        Project object = null;
+
+        try
+        {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            object = (Project) objectInputStream.readObject();
+
+            this.projectName = object.getProjectName();
+            this.productName = object.getProductName();
+            this.creatorName = object.getCreatorName();
+            this.comments = object.getComments();
+            this.language = object.getLanguage();
+            this.functionPoint = object.getFunctionPoint();
+
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public String getProjectName()
@@ -67,5 +107,41 @@ public class Project
     public void setLanguage(String language)
     {
         this.language = language;
+    }
+
+    public FunctionPoint getFunctionPoint()
+    {
+        return functionPoint;
+    }
+
+    public void setFunctionPoint(FunctionPoint functionPoint)
+    {
+        this.functionPoint = functionPoint;
+    }
+
+    /**
+     * Method to write to a file
+     * @param fileName the file name to write to
+     */
+    public void writeToFile(String fileName)
+    {
+        try
+        {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            objectOutputStream.writeObject(this);
+
+            objectOutputStream.close();
+            fileOutputStream.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
