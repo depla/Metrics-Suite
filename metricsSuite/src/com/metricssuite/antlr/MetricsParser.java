526cc1d9fe2b61801462847be5876c7b175e6fbc
Dennis La <dennisla95@yahoo.com>
2020-04-01 13:49:50 -0700

    Create MetricsParser.java

---
+++ metricsSuite/src/com/metricssuite/antlr/MetricsParser.java
@@ -0,0 +1,15 @@
+package com.metricssuite.antlr;
+
+import java.io.File;
+import java.io.IOException;
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+
+public class MetricsParser
+{
+    public static void parse(File[] selectedFiles)
+    {
+
+    }
+}
